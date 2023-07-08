2b3b6faca9c6d460ff8af3f92bc3ce62a7bdb968
alexander <lokistapway@yandex.ru>
2023-07-08 20:22:45 +0300

    Добавлен функционал проверки кода подтверждения перед регистрацией

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt'
 import dotenv from 'dotenv'
 import User from "../models/UserModel.js"
 import jwt from 'jsonwebtoken'
+import EmailCodes from "../models/EmailCodesModel.js";
 
 dotenv.config()
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
@@ -30,7 +31,7 @@ export const login = async (req, res) => {
 }
 
 export const registration = async (req, res) => {
-    const { userName, email, password, confirmPassword } = req.body
+    const { userName, email, password, confirmPassword, confirmCode } = req.body
 
     try {
         const user = await User.findOne({ email });
@@ -40,11 +41,23 @@ export const registration = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 12);
 
+        const emailCodeList = await EmailCodes
+            .find({ email })
+            .sort({createdAt:-1})
+            .limit(1)
+        const lastEmailCode = emailCodeList[0];
+
+        if (lastEmailCode.used) return res.status(400).json({ message: 'Код уже использован' })
+        if (lastEmailCode.code !== confirmCode) return res.status(400).json({ message: 'Неверный код' })
+        if (lastEmailCode.endTime < new Date()) return res.status(400).json({ message: 'Время кода истекло' })
+
         const createdUser = await User.create({
             userName,
             email,
             password: hashedPassword,
         })
+
+        await EmailCodes.updateOne(lastEmailCode, { $set: { used: true }})
 
         const token = jwt.sign(
             {id: createdUser._id, email: createdUser.email },
@@ -57,4 +70,41 @@ export const registration = async (req, res) => {
         console.log(e)
         res.status(500).json({ message: 'Произошла ошибка' })
     }
+}
+
+export const sendConfirmationCode = async (req, res) => {
+    const codeDuration = 3;
+    const sendLimitSeconds = 90;
+
+    const { email } = req.body;
+
+    if (!email) res.status(400).json({ error: 'Емейл не передан' })
+
+    const foundRecords = await EmailCodes.find({ email })
+    const dates = foundRecords
+        .map(record => record.createdAt.getTime())
+
+    if (dates.length !== 0) {
+        const lastTime = dates.reduce((x, y) => Math.max(x, y))
+        const currentTime = new Date().getTime()
+
+        if (lastTime + (sendLimitSeconds * 1000) > currentTime) {
+            return res.status(400).json({ error: 'Повторное подтверждение доступно через 90 секунд' })
+        }
+    }
+
+    const randomNumber = Math.random().toString().slice(-4)
+    const currentDate = new Date()
+    const endTime = currentDate.setMinutes(currentDate.getMinutes() + codeDuration)
+
+    await EmailCodes.create({
+        email,
+        code: randomNumber,
+        used: false,
+        endTime,
+    })
+
+    console.log(randomNumber);
+
+    res.status(200).json({ success: true });
 }
