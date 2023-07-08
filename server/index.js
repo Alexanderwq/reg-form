68b5e1a8004cc7076b01a08ea768274d2359c9f5
alexander <lokistapway@yandex.ru>
2023-07-08 12:54:28 +0300

    В файл сервера добавлены основные настройки

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express'
+import mongoose from "mongoose";
+import routes from './routes/routes.js'
 
-app.listen(port, () => {
-    console.log(`listen ${port}`)
-})
+const app = express()
+
+app.use((express.json({ limit: "30mb", extended: true})))
+app.use((express.urlencoded({ limit: "30mb", extended: true})))
+
+app.use(routes)
+
+const DB_URL = process.env.DB_URL
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(
+    DB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
