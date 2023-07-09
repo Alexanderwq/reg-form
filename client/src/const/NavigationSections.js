4c7d1518fecd976312d4709e8ef7eab8a39228d5
alexander <lokistapway@yandex.ru>
2023-07-09 16:18:11 +0300

    Создан класс с названиями секций

---
+++ client/src/const/NavigationSections.js
@@ -0,0 +1,17 @@
+export default class NavigationSections{
+    static get SIGN_UP() {
+        return 'signup'
+    }
+
+    static get SIGN_IN() {
+        return 'signin'
+    }
+
+    static get CONFIRMATION() {
+        return 'confirmation'
+    }
+
+    static get PROFILE() {
+        return 'profile'
+    }
+}
