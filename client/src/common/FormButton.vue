57d2c941c604549c4b197f305167dee08424a162
alexander <lokistapway@yandex.ru>
2023-07-09 11:59:39 +0300

    Создана кнопка для форм

---
+++ client/src/common/FormButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="form-button" @click="$emit('click')">
+    <slot name="text"></slot>
+  </button>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+  .form-button{
+    border-radius: 10px;
+    background: #4786FF;
+    color: #fff;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    transition: .2s ease-in-out;
+    cursor: pointer;
+    border: 2px solid #4786FF;
+    padding: 18px 80px;
+    &:hover{
+      border-radius: 10px;
+      background: transparent;
+      border: 2px solid #4786FF;
+      color: #4786FF;
+    }
+  }
+</style>
